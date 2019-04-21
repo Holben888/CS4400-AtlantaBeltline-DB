7a3cd9ce763c9715059d9c1b56f62fc89993bb1d
Holben888 <holben888@gmail.com>
2019-04-21 14:38:30 -0400

    finish transit history

--- server/routes/transit.js
+++ server/routes/transit.js
@@ -2,6 +2,33 @@ const express = require('express')
 const router = express.Router()
 const query = require('./connection')
 
+router.get('/history', async (req, res, err) => {
+  const transits = await query(`SELECT h.transitdate, h.transitroute, h.transittype, t.transitprice
+  FROM transit AS t join usertransittaken AS h
+  ON t.transittype=h.transittype AND t.transitroute=h.transitroute
+  WHERE h.username=\"${req.query.username}\"
+  ${
+    req.query.orderBy
+      ? `ORDER BY ${
+          req.query.orderBy === 'transitroute'
+            ? 't.' + req.query.orderBy
+            : 'h.' + req.query.orderBy
+        } ${req.query.sort}`
+      : ''
+  }
+  `)
+
+  if (!transits) res.sendStatus(400)
+  else
+    res.send({
+      transits: transits.map(transit => {
+        const d = new Date(transit.transitdate)
+        transit.transitdate = d.toLocaleDateString('en-US')
+        return transit
+      }),
+    })
+})
+
 router.get('/', async (req, res, err) => {
   const transits = await query(
     `SELECT transitroute, transittype, transitprice, (SELECT COUNT(*)
